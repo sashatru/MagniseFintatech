275c4772ceb406a17f303a7e4f9fc1eee8d3814c
Oleksandr Trubnikov <o.trubnikov@newlinetechnologies.net>
2024-10-27 09:23:25 +0200

    InstrumentSpinner & SubscribeButton #1

--- app/src/main/java/com/magnise/fintatech/presentation/ui/screens/SubscribeButton.kt
+++ app/src/main/java/com/magnise/fintatech/presentation/ui/screens/SubscribeButton.kt
@@ -1,11 +1,8 @@
 package com.magnise.fintatech.presentation.ui.screens
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,7 +17,7 @@ fun SubscribeButton(
     onSubscribeClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Button(
+    OutlinedButton(
         onClick = onSubscribeClick,
         modifier = modifier.width(120.dp)
     ) {
